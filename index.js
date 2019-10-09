d594aa0952e43f2e9e918e8a377e2e32718077e5
Casper Norspang <casper@norspang.dk>
2019-10-09 19:55:35 +0200

    Added functionality

--- index.js
+++ index.js
@@ -10,26 +10,33 @@ bot.on('ready', () => {
 
 //Svare tilbage med 'Fader Kurt'
 bot.on('message', msg=>{
+    if (msg.author.bot) return;
     if(msg.content === "!FaderKurt"){
+        console.log('Udføre: FaderKurt')
         msg.reply("\nFader Kurt\nDu som er på uni\nHellige pointer din addresse\nKomme din reference\nSke din vilje som i koden,\nsåledes også i compileren\nGiv os i dag vores daglige malloc\nog forlad os vores segmentation - faults,\nsom også vi forlader vores medprogrammører\nThi dit er C, og Emacs, og IMPR\nI alt evighed\nAmen.");
     }
 })
 
 //Forklare kort sproget C
 bot.on('message', msg=>{
+    if (msg.author.bot) return;
     if(msg.content === "!c"){
+        console.log('Udføre: C\'s hellige sætning')
         msg.reply("C is terse, low-level and permessive");
     }
 })
 
 //Rykker KurtBots briller op eller ned
 bot.on('message', msg=>{
+    if (msg.author.bot) return;
     if(msg.content.toLowerCase() === "!rykbrillerop" || msg.content.toLowerCase() === "!rykbrillerned"){
         msg.reply("*click*")
         if(msg.content.toLowerCase() === "!rykbrillerop"){
+            console.log('Udføre: Ryk briller op')
             brillestatus = "oppe"
         }
         else{
+            console.log('Udføre: Ryk briller ned')
             brillestatus = "nede"
         }
     }
@@ -37,22 +44,64 @@ bot.on('message', msg=>{
 
 //Svare på hvor på hovdet KurtBots briller sidder
 bot.on('message', msg=>{
+    if (msg.author.bot) return;
     if(msg.content === "!brillestatus"){
+        console.log('Udføre: Brillestatus')
         msg.reply(brillestatus)
     }
 })
 
 //Bruger du en IDE? Kurt græder
-/*bot.on('message', msg=>{
-    if(msg.content.includes('IDE') || msg.content.tolowercase.includes('vscode') || msg.content.toLowerCase.includes('visual studio')|| msg.content.toLowerCase.includes('sublime')){
+bot.on('message', msg=>{
+    if (msg.author.bot) return;
+    if(msg.content.toLowerCase().includes('IDE') || msg.content.toLowerCase().includes('vscode') || msg.content.toLowerCase().includes('visual studio')|| msg.content.toLowerCase().includes('sublime')){
+        console.log('Udføre: Bible_kurt')
         msg.react("624580114390319134")
     }
-})*/
+})
 
 bot.on('message', msg =>{
+    if (msg.author.bot) return;
+    if(msg.content.toLowerCase() === "!hjælp"){
+        console.log('Udføre: Hjælpemenu')
+        msg.reply('Her er en liste over ting jeg kan hjælpe med, du skal bare skrive !hjælp "emne"\n\
+        Scanf\n\
+        Prinf\n\
+        Konstant\n\
+        Modulo(%)\n\
+        Inkrement(++)\n\
+        Dekrement(--)\n\
+        ')
+    }
+})
+
+//Svare tilbage med et link til en specifik funktion i C
+bot.on('message', msg =>{
+    if (msg.author.bot) return;
     if(msg.content.toLowerCase().includes('!hjælp')){
-        if (msg.content.content.toLowerCase().includes('scanf')){
-            msg.reply('')
+        if (msg.content.toLowerCase().includes('scanf')){
+            console.log('Udføre: Scaf link')
+            msg.reply('Her er en guide til hvordan man kan bruge scanf: https://youtu.be/IDLNZ82ar9w')
+        }
+        if (msg.content.toLowerCase().includes('printf')){
+            console.log('Udføre: Printf link')
+            msg.reply('Her er en guide til hvordan man kan bruge printf: https://youtu.be/c3phCPF-Hx8')
+        }
+        if (msg.content.toLowerCase().includes('konstant')){
+            console.log('Udføre: Konstant link')
+            msg.reply('Her er en guide til hvordan man kan bruge konstanter i C: https://youtu.be/1LsPv13-i80')
+        }
+        if (msg.content.toLowerCase().includes('modulo') || msg.content.toLowerCase().includes('%')){
+            console.log('Udføre: Modulo link')
+            msg.reply('Her er en guide til hvordan man kan bruge modulo(%) operatoren i C: https://youtu.be/EHHybuqQgbA')
+        }
+        if (msg.content.toLowerCase().includes('inkrement') || msg.content.toLowerCase().includes('dekrement') || msg.content.toLowerCase().includes('--') || msg.content.toLowerCase().includes('++')){
+            console.log('Udføre: Inkrement og dekrement link')
+            msg.reply('Her er en guide til hvordan du kan bruge inkrement(++) og dekrement(--) operatorene i C: https://youtu.be/rVqDNi5FUug')
+        }
+        if (msg.content.toLowerCase().includes('and') || msg.content.toLowerCase().includes('&&') || msg.content.toLowerCase().includes('or') || msg.content.toLowerCase().includes('||')){
+            console.log('Udføre: Logiske operatore link')
+            msg.reply("Her er en guide til hvordan du kan bruge logiske operatore i C: https://youtu.be/e5kPsWhpY4A")
         }
     }
 })
